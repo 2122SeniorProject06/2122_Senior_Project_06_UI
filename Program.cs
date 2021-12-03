83fa06221ac6e162e37c43ce86cbdddfbc4a97eb
HugoMazari <hugojrh@nevada.unr.edu>
2021-12-02 18:31:14 -0800

    Started backend project. Included git ignore.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2122_Senior_Project_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working Message!");
+        }
+    }
+}
