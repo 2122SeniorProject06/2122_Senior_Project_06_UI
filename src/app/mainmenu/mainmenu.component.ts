2540c33570a10cecbb8a30835e5249111be255a5
Daniela Martinez <dmartinez@nevada.unr.edu>
2021-12-06 17:31:16 -0800

    Adding main menu UI

---
+++ src/app/mainmenu/mainmenu.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-mainmenu',
+  templateUrl: './mainmenu.component.html',
+  styleUrls: ['./mainmenu.component.css']
+})
+
+export class MainmenuComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+  }
+
+  gotoCalmMusic(){
+    this.router.navigateByUrl('./calmmusic-component');
+  }
+
+  gotoCountGame(){
+    this.router.navigateByUrl('./counting-component');
+  }
+
+  gotoDeepBreath(){
+    this.router.navigateByUrl('./deepbreathing-component');
+  }
+
+  gotoFiveOne(){
+    this.router.navigateByUrl('./fivesees');
+  }
+
+  gotoNoteTake(){
+    this.router.navigateByUrl('./notetaking-component');
+  }
+}
