4aab7cd8c8b43c64be4206d5d4a2fbaa9371bb27
goolsbysarah <sgoolsby@nevada.unr.edu>
2022-02-23 16:34:49 -0800

    add grid code to mainmenu css and html, needs editting by dani

--- src/app/mainmenu/mainmenu.component.ts
+++ src/app/mainmenu/mainmenu.component.ts
@@ -1,7 +1,7 @@
 //Created by Daniela Martinez
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+//this will need to be edited to route the dynamic grid to the correct components, i added the html and css to the correct files
 @Component({
   selector: 'app-mainmenu',
   templateUrl: './mainmenu.component.html',
