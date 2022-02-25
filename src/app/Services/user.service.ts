07d153f250b2111f93d0e4339b842c818362841e
goolsbysarah <sgoolsby@nevada.unr.edu>
2022-02-24 17:14:04 -0800

    work on api services

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,28 +1,29 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { UserLogin, UserRegistration } from '../../../Models/UserModels'
 
 @Injectable({
   providedIn: 'root'
 })
-
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 export class UserService {
+constructor(private http: HttpClient){}
+
 
  registerUser!: UserRegistration;
  loginUser!: UserLogin;
 
 login(loginModel: UserLogin){
   //once api is set up in backend, this function will make an http POST call
-  if (loginModel.username != null && loginModel.password != null) {
-    return true;
-  }
-  return false;
+  //need the login url
+return this.http.post(loginUrl, loginModel, httpOptions);
 }
  register(registerModel: UserRegistration){
    //once api is set up in backend this function will an http POST call to add user to database
-   if(registerModel.username != null && registerModel.email != null && registerModel.password != null){
-     return true;
-   }
-   return false;
+   //need the register url
+  return this.http.post(loginUrl, registerModel, httpOptions);
  }
 
 
