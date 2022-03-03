5ee11e2b15ef23fc2c7d017807c556e04260d060
goolsbysarah <sgoolsby@nevada.unr.edu>
2022-03-02 22:33:14 -0800

    fixed dataSource bug

--- src/app/Features/ViewJournal/viewjournal.component.ts
+++ src/app/Features/ViewJournal/viewjournal.component.ts
@@ -1,7 +1,8 @@
 import { JournalService } from './../../Services/journal.service';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import { MatTableModule, MatCellDef, MatColumnDef, MatTable } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 
 @Component({
@@ -15,9 +16,10 @@ export class ViewJournalComponent  {
   constructor(private JournalService: JournalService) {}
 
   displayedColumns = ['date', 'title', 'entry'];
-  dataSource?: MatTableDataSource<JournalModel>;
+  dataSource = new MatTableDataSource<JournalModel>();
   journals?: JournalModel[];
   userId?: number; //this will be the user's id when they log in
+ // @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -25,11 +27,15 @@ export class ViewJournalComponent  {
     this.getJournals();
   }
 
+  ngAfterViewInIt() {
+    //this.dataSource.paginator = this.paginator;
+  }
     getJournals() {
+      //right now userid is null
       this.JournalService.getJournal(this.userId).subscribe(journals => {
         this.journals = journals as JournalModel[];
         if (this.journals){//if i receive data
-        this.dataSource = new MatTableDataSource<JournalModel>(this.journals);
+        this.dataSource.data = this.journals;
         }
         //add if i don't receive data
       })
