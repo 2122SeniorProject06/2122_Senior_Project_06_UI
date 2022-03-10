04b9f75ed69c8aeceac71d778e20452b9b513c52
HugoMazari <hugojrh@nevada.unr.edu>
2022-03-09 17:50:31 -0800

    Revert "Started version two of audio meditation"
    This reverts commit 574c7716933aaba5b0e511aee0828f5eee9f32c9.

--- src/app/Features/AudioMeditation/audio-meditation.component.ts
+++ src/app/Features/AudioMeditation/audio-meditation.component.ts
@@ -6,33 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./audio-meditation.component.css']
 })
 export class AudioMeditationComponent {
-  title = 'AudioMeditation';
-  audio: HTMLAudioElement;
-  isPlaying: boolean;
-  
-  constructor(){
-    this.isPlaying = false;
-    this.audio = new Audio();
-    //this.audio.src = "./audio-meditation.mp3";
-    this.audio.load();
-    this.audio.addEventListener('ended', function(){
-      this.currentTime = 0;
-      this.play();
-    }, false);
-  }
-
-  modifyAudio(){
-    if(!this.isPlaying)
-    {
-      console.log("We don't talk about bruno");
-     // this.audio.play();
-      this.isPlaying = true;
-    }
-    else
-    {
-      console.log("No se habla de bruno");
-      //this.audio.pause();
-      this.isPlaying = false;
-    }
-  }
+  title = 'audioMeditation';
 }
