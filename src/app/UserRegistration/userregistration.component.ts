6319f1e6612381aead5e2fe1f42d5eeb2faceb19
HugoMazari <56401629+HugoMazari@users.noreply.github.com>
2022-04-10 11:37:03 -0700

    Updated New Account Creation Page (#11)
    * Update userregistration.component.ts
    
    Began writing for error handling in user registration. Currently I have no idea how to update the registration page given an error. Comments in code provide further description.
    
    No error handling is needed in other functionalities at the moment.
    
    * Error handling for Registration V2
    
    Was able to figure out how to use Validators similar to the Ground function. Still having issues with making the errors conditional and outputting specific messages to user if input is invalid.
    More information is in userregistration.component.ts on line 83.
    
    * Error handling update
    
    Pair programmed with the great Hugo. Able to get solid outputs for each registration error. Currently working on optimization and Null handling.
    
    * Updated user registration to match main menu theme and converted to flex display.
    
    Now to figure out the issue with the passing the UserLogin.
    
    * Error handling update
    
    Pair programmed with Hugo. Finished output for registration errors. now need to work on color and visual errors for other components.
    
    * Added a png of logo only
    
    Co-authored-by: Andrew Bevilacqua <drewbevimail@gmail.com>

--- src/app/UserRegistration/userregistration.component.ts
+++ src/app/UserRegistration/userregistration.component.ts
@@ -1,36 +1,71 @@
 import { UserRegistration } from '../../../Models/UserModels';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from "../Services/user.service"
+import { UserService } from "../Services/user.service";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Animations } from 'animations';
+import { HostListener } from '@angular/core';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'user-registration',
+  animations: [Animations.loadingTrigger],
   templateUrl: './userregistration.component.html',
   styleUrls: ['./userregistration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
+  
+  showLoading: boolean = false; // Shows the loading bar.
+  targetEvent: HTMLElement = document.createElement('br'); // The actual target to load.
+  bgImage: HTMLElement | null = document.createElement('br');
+
   form!: FormGroup;
-   confirmedPassword?: string;
+  confirmedPassword?: string;
   isRegistered: boolean = false;
+  notRegistered: boolean = false;
+  passVerification: boolean[] = [true,true,true,true];
+  ifNull: boolean[] = [false,false,false,false];
+  errorResults: boolean[] = [true,true,true,true];
+  errorMessage: string[] = ["", "", "", ""];
   registerModel: any;
   registerForm?: any;
-  constructor(private UserService: UserService, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute) {}
-
   registerUser!: UserRegistration;
+  errorMessages: string[] = ["","","",""]
+  passwordErrorTypes: string[] = ["- The password must be at least more than 8 lengths.",
+                                  "- The password must contain at least one lowercase character.",
+                                  "- The password must contain at least one capital character.",
+                                  "- The password must contain at least one number." ]
+
+  constructor(
+    private UserService: UserService, 
+    private formBuilder: FormBuilder, 
+    private snackBar: MatSnackBar, 
+    private router: Router, 
+    private route: ActivatedRoute) { }
+
   ngOnInit() {
+    console.log("OnInit");
+
+    this.bgImage = document.getElementById("bg-image");
+    this.bgImage!.style.filter = "blur(8px)";
+
     this.form = this.formBuilder.group({
-      Email: [''],
-      Username: [''],
-      Password: [''],
-      confirmpassword: ['']
+      Email: ['', Validators.required ],
+      Username: ['', Validators.required ],
+      Password: ['', Validators.required ],
+      confirmpassword: ['', Validators.required ],
     });
   }
   get f() { return this.form.controls }
 
-/*Pair Programmed with Andrew, he was helpful with his knowledge of the API controllers*/
-
+/*
+  Pair Programmed with Andrew, he was helpful with his knowledge of the API controllers
+  Registration error handling pair programmed with Andrew, Sarah, and Hugo
+*/
+  /**
+   * Very
+   */
   Register() {
     const registerForm = new FormData();
     registerForm.append('Email', this.form.get('Email')?.value);
@@ -42,24 +77,60 @@ export class UserRegistrationComponent implements OnInit {
     registerModel.Email = this.form.get('Email')?.value;
     registerModel.Username = this.form.get('Username')?.value;
     registerModel.Password = this.form.get('Password')?.value;
-    this.confirmedPassword = this.form.get('confirmpassword')?.value;
-//if the confirmed password is equal to the other password, then we create the user
-    if(this.confirmedPassword == registerModel.Password){
-      this.UserService.register(registerModel).subscribe((result: any) =>{
-        if(result) {
-          console.log("Registration was successful");
-          this.goToLogin();
-        }
-      })
-    }
+    registerModel.confirmedPassword = this.form.get('confirmpassword')?.value;
+
+    this.UserService.register(registerModel).subscribe((result: any) =>{
+      if(!result.verificationResults.includes(false)) { //if result all true then no error occured and account is created
+        this.isRegistered = true;
+        this.notRegistered = false;
+        console.log(result.verificationResults);
+        this.goToLogin();
+      }
+      else{ //if one result is false/user not registered
+        this.errorResults = result.verificationResults;
+        this.errorMessages = result.verificationErrors
+        console.log(this.errorResults);
+        console.log(this.errorMessages);
+        
+        this.isRegistered = false;
+        this.notRegistered = true;
+        
+      } 
+    })
 
   }
+
   goToLogin(){
-    this.router.navigateByUrl('login');
+    this.activateLoadingAnimation('/login', "LOGIN");
+    this.onPopState(new Event('Changing Screens'))
   }
 
   goToMainMenu(){
-    this.router.navigateByUrl('main-menu');
+    this.activateLoadingAnimation('/main-menu', "MAIN MENU");
+    this.onPopState(new Event('Changing Screens'))
   }
-}
 
+  /**
+  * Creates a html element to send to the loading animation to route to the next page.
+  * @param routeLink The route path to take.
+  * @param routeName The name of the activity to go to.
+  */
+  activateLoadingAnimation(routeLink: string, routeName: string){
+    let mainMenuEvent = document.createElement('p');
+    let mainMenuParent = document.createElement('div');
+    mainMenuParent.id = routeLink;
+    mainMenuEvent.innerHTML = routeName;
+    mainMenuParent.appendChild(mainMenuEvent);
+    this.targetEvent = mainMenuEvent;
+    this.showLoading = true;
+  }
+
+  /**
+     * Unblurs the background image on leaving the page.
+     * @param event Event that triggers the unblurring.
+     */
+   @HostListener('window:popstate', ['$event'])
+   onPopState(event : Event) {
+       this.bgImage!.style.filter = '';
+   }
+}
