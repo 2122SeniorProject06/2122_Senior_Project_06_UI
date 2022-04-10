ddbe7ebfbc792cf75a6b3222502e1c44adf90f3a
Daniela Martinez <dmartinez@nevada.unr.edu>
2022-04-10 13:15:32 -0700

    Wrong Button - turns red
    if wrong button is clicked, turns red and shows answer

--- src/app/Features/CountGame/countgame.component.ts
+++ src/app/Features/CountGame/countgame.component.ts
@@ -20,5 +20,34 @@ export class CountGameComponent {
     this.router.navigateByUrl('/main-menu');
   }
 
-  
+  newColor = false;
+  toggleColorOne() {
+    this.newColor = !this.newColor;
+    this.newColor = true;
+  }
+
+  newColorTwo=false;
+  toggleColorTwo() {
+    this.newColorTwo = !this.newColorTwo;
+    this.newColorTwo = true;
+  }
+
+  newColorThree=false;
+  toggleColorThree() {
+    this.newColorThree = !this.newColorThree;
+    this.newColorThree = true;
+  }
+
+
+  newColorFour=false;
+  toggleColorFour() {
+    this.newColorFour = !this.newColorFour;
+    this.newColorFour = true;
+  }
+
+  newColorFive=false;
+  toggleColorFive() {
+    this.newColorFive = !this.newColorFive;
+    this.newColorFive = true;
+  }
 }
