58bb716fca7385a78e147c5283ad77b9ef38b964
Daniela Martinez <dmartinez@nevada.unr.edu>
2022-04-12 15:03:25 -0700

    Encourage Updated
    Encourage Updated

--- src/app/Features/Mantra/mantra.component.ts
+++ src/app/Features/Mantra/mantra.component.ts
@@ -1,28 +1,29 @@
 import {Component} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
-/**
- * Value Selection
- */
 
 @Component({
   selector: 'Mantra',
   templateUrl: './mantra.component.html',
   styleUrls: ['./mantra.component.css'],
 })
-export class MantraComponent {
 
+export class MantraComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
   ){}
   
+  //Routes to Main Menu
   goToMain() {
     this.router.navigateByUrl('/main-menu');
   }
 
+  //Refresh Page
+  restartFeature() {
+    window.location.reload();
+  }
   
+  //Display Date
+  currentDate = new Date();
 }
-
-const SAMPLE_TEXT="I am a Champion for __________ .I am Worthy of ___________ My Wealth is in my ____________ My _________ has no limits. I’m invested in ___________ I will __________ I want ___________"
-
